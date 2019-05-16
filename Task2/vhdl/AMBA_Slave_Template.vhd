ef6ec014797319bb3d84fd4b5a5080b7e50a6b44
ggland <ggland@gitlab.com>
2019-05-16 22:45:34 +0200

    me stupido. doing it now the easy way

--- Task2/vhdl/AMBA_Slave_Template.vhd
+++ Task2/vhdl/AMBA_Slave_Template.vhd
@@ -32,28 +32,72 @@ architecture Behaviour of Amba_Slave is
 	signal state : state_type_slave;
 
 	signal int_data : std_logic_vector(bus_size-1 downto 0);
+	signal enable_write	: std_logic;
+	signal enable_read	: std_logic;
 begin
 
-ASYNC: process (in_PENABLE, in_PCLK, in_PRESETn)
+enable_write <= in_PENABLE and in_PWRITE and in_PSELx;
+enable_read  <= not in_PWRITE and in_PSELx;	-- data is read everytime to be ready on the first cycle
+
+out_PREADY <= int_pready; --int_pready_read or int_pready_write;
+
+WRITE: process(in_PRESETn, enable_write)
 begin
-	if rising_edge (in_PENABLE) then	-- rising_edge penable
-		case in_PADDR is
-			when '0' =>
-				if in_PWDATA = '1' then
-					signal
-				else
+	if in_PRESETn ='1' then
+		int_data <= x"0000";
 
-				end if;
-			when '1' =>
-				if in_PWDATA = '1' then
-					signal
-				else
+	elsif rising_edge(enable_write) then
+		int_data <= in_PWDATA;
+	end if;
+end process WRITE;
 
-				end if;
-		end case; -- in_PADDR
-	end if;	
-	
-end process ASYNC;
+READ: process(in_PRESETn, enable_read)
+begin
+	if in_PRESETn ='1' then
+		int_data <= x"0000";
+
+	elsif rising_edge(enable_read) then
+		out_PRDATA <= int_data;
+
+	end if;
+end process WRITE;
+
+-- process to forward the data to whatever module or to write into the read register
+SFORWARD: process(in_PRESETn, in_PCLK)
+begin
+
+	if rising_edge(in_PCLK) then
+		if enable_write or enable_read then
+			int_pready <= '1';
+		else 
+			int_pready <= '0';
+		end if; -- enable read or write
+
+		-- here some logic to compare the data ins with the other registers and update them
+	end if;
+		
+end process SFORWARD;
+
+--ASYNC: process (in_PENABLE, in_PCLK, in_PRESETn)
+--begin
+--	if rising_edge (in_PENABLE) then	-- rising_edge penable
+--		case in_PADDR is
+--			when '0' =>
+--				if in_PWDATA = '1' then
+--					signal
+--				else
+--
+--				end if;
+--			when '1' =>
+--				if in_PWDATA = '1' then
+--					signal
+--				else
+--
+--				end if;
+--		end case; -- in_PADDR
+--	end if;	
+--	
+--end process ASYNC;
 end behaviour;
 
 
